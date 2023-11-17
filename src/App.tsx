1350b83ac0ddda1b8da9b07a7955396097c87e9c
Sorin <sorin@q3.ro>
2023-11-17 23:06:34 +0200

    remove most text

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,20 @@ import Clock from './components/clock';
 function App() {
   return (
     <div className="text-center p-2">
-      <h1 className="font-bold text-3xl text-gray-800 my-2">
-        Create a Shot Clock{" "}
-        <span role="img" aria-label="Basketball">
+      <h1 className="font-bold text-3xl text-gray-800 my-10">
+        Shot 
+        <span role="img" aria-label="Basketball" className='inline-block mx-2'>
           🏀
         </span>
+        Clock
+        
       </h1>
-      <p className="my-2 border-t-2 p-4 mb-6">
-        A basketball field has 2 shot clocks, one above each rim. Both countdown 24 seconds.
-        Referees can stop and/or reset clocks when needed.
-      </p>
-      <div className="my-2 font-medium text-xl m-8">
-        <p className="bg-gray-800 text-white shadow-md rounded p-4">
-          As a user, I want to see 2 clocks with pause & reset buttons, so I can manage the game.
-        </p>
-        <p className="text-gray-700 italic text-sm p-2">
-          Feel free to create new files where needed. Using TypeScript is a nice-to-have. Tailwind
-          is available if you wish to use it.
-        </p>
-      </div>
-      <hr className="border" />
-      <div className='flex max-w-xl mx-auto'>
-        <div className='fle flex-grow'>
+
+      <div className='flex max-w-xl mx-auto rounded-xl bg-gray-200'>
+        <div className='flex-grow'>
           <Clock/>
         </div>
-        <div className='fle flex-grow'>
+        <div className='flex-grow'>
           <Clock/>
         </div>
       </div>
--- src/components/clock.js
+++ src/components/clock.js
@@ -25,7 +25,7 @@ const Clock = () => {
 
   return (
     <div className="bg-gray-200 py-10" role="timer" aria-live="polite" aria-atomic="true">
-      <div className={`text-6xl my-4 ${seconds < 6 ? 'text-red-700' : 'text-red-900'} font-mono cursor-default`}>{seconds}</div>
+      <div className={`text-6xl my-4 ${seconds < 6 ? 'text-red-700' : 'text-red-900'} ${seconds === 0 && 'animate-ping'} font-mono cursor-default`}>{seconds}</div>
       <div className="">
         <button onClick={() => setIsRunning(!isRunning)} className='bg-black text-white p-3 rounded-l-lg hover:bg-gray-800 text-3xl border-2 border-white'  aria-label={isRunning ? 'Pause' : 'Play'}>
           {isRunning ? <PiPauseFill /> : <PiPlayFill />}
