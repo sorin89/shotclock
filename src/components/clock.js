1426917beadb5d88c6b950bebb8cc0a35f6dbc3e
Sorin <sorin@q3.ro>
2023-11-17 22:47:10 +0200

    accessibility

--- src/components/clock.js
+++ src/components/clock.js
@@ -18,23 +18,19 @@ const Clock = () => {
     return () => clearInterval(timer);
   }, [isRunning, seconds]);
 
-  const Pause = () => {
-    setIsRunning(false);
-  };
-
   const Reset = () => {
     setSeconds(24);
     setIsRunning(true);
   };
 
   return (
-    <div className="bg-gray-200 py-10">
+    <div className="bg-gray-200 py-10" role="timer" aria-live="polite" aria-atomic="true">
       <div className={`text-6xl my-4 ${seconds < 6 ? 'text-red-700' : 'text-red-900'} font-mono cursor-default`}>{seconds}</div>
       <div className="">
-        <button onClick={() => setIsRunning(!isRunning)} className='bg-black text-white p-3 rounded-l-lg hover:bg-gray-800 text-3xl border-2 border-white'>
+        <button onClick={() => setIsRunning(!isRunning)} className='bg-black text-white p-3 rounded-l-lg hover:bg-gray-800 text-3xl border-2 border-white'  aria-label={isRunning ? 'Pause' : 'Play'}>
           {isRunning ? <PiPauseFill /> : <PiPlayFill />}
         </button>
-        <button onClick={Reset} className='bg-black text-white p-3 rounded-r-lg hover:bg-gray-700 text-3xl border-2 border-white'>
+        <button onClick={Reset} className='bg-black text-white p-3 rounded-r-lg hover:bg-gray-700 text-3xl border-2 border-white' aria-label='Reset'>
           <FaSyncAlt />
         </button>
       </div>
